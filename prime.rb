0121ece32efbc7a0f6c4b76a6bc543dcfd0d43ad
Becky Phan <beckyphan@live.com>
2019-05-29 04:09:23 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -5,11 +5,11 @@ def prime?(integer)
     return false 
   else 
     (2..integer).each do |number|
-      if integer % number == 0 
-        divisible_by.push(number) 
+      if integer % number == 0
+        divisible_by.push(number)
       else 
-        nil 
-      end 
+        false
+      end
     end 
     if divisible_by.length == 2
       return false
