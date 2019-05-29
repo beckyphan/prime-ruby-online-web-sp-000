ccbfe8266d6afc413c64d80ac51e9dae36142d69
Becky Phan <beckyphan@live.com>
2019-05-29 03:57:20 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
 def prime?(integer)
-    if integer < 0 
-      return false 
-    else
-      (2...integer).find do |number|
-        integer % number == 0 
-      end 
-    end
+  if integer < 0 
+     return false 
+  else
+    (2...integer).find do |number|
+      integer % number == 0 
+    end 
+  end
 end 
